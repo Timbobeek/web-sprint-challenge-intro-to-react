b77bfe4852b2941c3b734c3fba27e31ad66f57e7
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-12-10 03:01:59 -0600

    basic styling has begun

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledCharacter = styled.div`
+  padding: 1%;
+  width: 50%;
+  color: purple;
+  background-color: beige;
+`
 
 const Character = props => {
   const { name } = props;
 
   return(
-    <div className='name'>{name}</div>
+    <StyledCharacter>
+      <div className='name'>{name}</div>
+    </StyledCharacter>
   );
 }
 
